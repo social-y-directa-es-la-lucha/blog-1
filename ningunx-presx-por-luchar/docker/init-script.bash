0684ef4b912093973b22b77951094001dfb5a602
noone <noone@nowhere.no>
2021-12-06 12:23:30 -0300

    x

--- ningunx-presx-por-luchar/docker/init-script.bash
+++ ningunx-presx-por-luchar/docker/init-script.bash
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(ningunx-presx-por-luchar.info www.ningunx-presx-por-luchar.info)
+domains=(www.ningunx-presx-por-luchar.info)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="ozerno@protonmail.com" # Adding a valid address is strongly recommended
